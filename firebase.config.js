3fd2f5a13b2be48a8d99c78f49fcf321ed321df2
praveenpothiraj <praveenpothiraj123@gmail.com>
2023-03-26 16:32:04 +0530

    abu all completed

--- firebase.config.js
+++ firebase.config.js
@@ -1,50 +1,58 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  getDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  arrayUnion,
-  getDocs,
-  arrayRemove,
+    getFirestore,
+    collection,
+    getDoc,
+    doc,
+    setDoc,
+    updateDoc,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    query,
+    where,
 } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
 
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8wT7enCuEP2qRRG_i5e-BYGVVB_5jDyg",
-  authDomain: "student-certificates.firebaseapp.com",
-  projectId: "student-certificates",
-  storageBucket: "student-certificates.appspot.com",
-  messagingSenderId: "991849254686",
-  appId: "1:991849254686:web:b9fb5e6868a33d1b7d030f",
+    apiKey: "AIzaSyA8wT7enCuEP2qRRG_i5e-BYGVVB_5jDyg",
+    authDomain: "student-certificates.firebaseapp.com",
+    projectId: "student-certificates",
+    storageBucket: "student-certificates.appspot.com",
+    messagingSenderId: "991849254686",
+    appId: "1:991849254686:web:b9fb5e6868a33d1b7d030f",
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 export {
-  db,
-  collection,
-  getDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  getDocs,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  updateProfile,
-  auth
+    db,
+    collection,
+    getDoc,
+    doc,
+    setDoc,
+    updateDoc,
+    query,
+    signOut,
+    where,
+    getDocs,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    updateProfile,
+    addDoc,
+    deleteDoc,
+    auth,
 };
--- script.js
+++ script.js
@@ -1,156 +1,180 @@
-import { auth, onAuthStateChanged } from "./firebase.config.js";
+import {
+    auth,
+    onAuthStateChanged,
+    db,
+    collection,
+    doc,
+    addDoc,
+    query,
+    deleteDoc,
+    where,
+    signOut,
+    updateDoc,
+    getDocs,
+} from "./firebase.config.js";
 
 const userState = document.getElementById("authuser");
 
+let status = "create";
+
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    userState.textContent = "Login as "+user.displayName;
-    console.log(user.displayName)
-  } else {
-  }
+    if (user) {
+        userState.textContent = "Login as " + user.displayName;
+        console.log(user.displayName);
+        getCredentials();
+    } else {
+    }
 });
 
 const alertBox = document.getElementsByClassName("alertMain")[0];
 function toggleAlert() {
-  if (alertBox.style.display === "none") {
-    alertBox.style.display = "flex";
-  } else alertBox.style.display = "none";
-  getCredentials();
+    if (alertBox.style.display === "none") {
+        alertBox.style.display = "flex";
+    } else alertBox.style.display = "none";
+    getCredentials();
 }
-//
+
 let userData = [];
 
-// function getCredentials() {
-//   fetch("http://localhost:8000/superclub/getuserdata")
-//     .then((data) => {
-//       return data.json();
-//     })
-//     .then((info) => {
-//       userData = info;
-//       showUser();
-//     });
-// }
+async function getCredentials() {
+    const q = query(
+        collection(db, "students"),
+        where("facultyid", "==", auth.currentUser.uid)
+    );
+
+    const querySnapshot = await getDocs(q);
+    userData.length = 0;
+
+    querySnapshot.forEach((doc) => {
+        userData.push({ ...doc.data(), sid: doc.id });
+    });
+    showUser();
+    console.log(userData);
+}
+
+async function studentDetailsUpdate(sid, data) {}
 
 // clear the form
 const formData = (user = "", passwd = "", mail = "") => {
-  const username = document.getElementById("username");
-  const password = document.getElementById("password");
-  const email = document.getElementById("email");
-  username.value = user;
-  password.value = passwd;
-  email.value = mail;
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const email = document.getElementById("email");
+    username.value = user;
+    password.value = passwd;
+    email.value = mail;
 };
 
 const table = document.getElementsByClassName("userTable")[0];
 function showUser() {
-  table.innerHTML = "";
-  const title = ["ID", "Username", "Password", "Email", "Edit", "Trash"];
-  let indexid = 1;
-  for (let i = 0; i < userData.length; i++) {
-    const tr = document.createElement("tr");
-    if (i === 0) {
-      const tr = document.createElement("tr");
-      for (let i = 0; i < title.length; i++) {
-        const th = document.createElement("th");
-        th.append(title[i]);
-        tr.append(th);
-      }
-      table.append(tr);
-    }
-    Object.keys(userData[i]).forEach((key, index) => {
-      const td = document.createElement("td");
-      td.append(index === 0 ? indexid++ : userData[i][key]);
-      tr.append(td);
-    });
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const butn1 = document.createElement("button");
-    butn1.append("Edit");
-    butn1.classList.add("userButn");
-    butn1.classList.add("editButn");
-    butn1.addEventListener("click", () => {
-      const uid = document.getElementById("uid");
-      const action = document.getElementById("action");
-      const text = userData[i]["uid"];
-      uid.value = text;
-      action.value = "update";
-      formData(
-        userData[i]["username"],
-        userData[i]["password"],
-        userData[i]["email"]
-      );
-      toggleAlert();
-    });
-    td1.append(butn1);
-    const butn2 = document.createElement("button");
-    butn2.append("Trash");
-    butn2.classList.add("userButn");
-    butn2.classList.add("trashButn");
-    butn2.addEventListener("click", () => {
-      const text = userData[i]["uid"];
-      const urlencode = `action=trash&uid=${text}`;
-
-      fetch("http://localhost:8000/superclub/newuser", {
-        method: "POST",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        body: encodeURI(urlencode),
-      })
-        .then((res) => {
-          return res.text();
-        })
-        .then((text) => {
-          console.log(text);
-          getCredentials();
+    table.innerHTML = "";
+    let indexid = 1;
+    for (let i = 0; i < userData.length; i++) {
+        const tr = document.createElement("tr");
+        if (i === 0) {
+            const tr = document.createElement("tr");
+            ["ID", "Username", "Password", "Email", "Edit", "Trash"].forEach(
+                (text) => {
+                    const th = document.createElement("th");
+                    th.append(text);
+                    tr.append(th);
+                }
+            );
+            table.append(tr);
+        }
+        [
+            indexid++,
+            userData[i].username,
+            userData[i].password,
+            userData[i].email,
+        ].forEach((key) => {
+            const td = document.createElement("td");
+            td.append(key);
+            tr.append(td);
         });
-    });
-    td2.append(butn2);
-    tr.append(td1);
-    tr.append(td2);
-    table.append(tr);
-  }
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const butn1 = document.createElement("button");
+        butn1.append("Edit");
+        butn1.classList.add("userButn");
+        butn1.classList.add("editButn");
+        butn1.addEventListener("click", () => {
+            formData(
+                userData[i]["username"],
+                userData[i]["password"],
+                userData[i]["email"]
+            );
+            toggleAlert();
+            status = userData[i].sid;
+        });
+        td1.append(butn1);
+        const butn2 = document.createElement("button");
+        butn2.append("Trash");
+        butn2.classList.add("userButn");
+        butn2.classList.add("trashButn");
+        butn2.addEventListener("click", async () => {
+            await deleteDoc(doc(db, "students", userData[i].sid));
+            console.log(userData[i].username, "deleted success");
+            getCredentials();
+        });
+        td2.append(butn2);
+        tr.append(td1);
+        tr.append(td2);
+        table.append(tr);
+    }
 }
 
 const form = document.getElementById("userform");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const username = e.target.username.value;
-  const password = e.target.password.value;
-  const email = e.target.email.value;
-  const action = e.target.action.value;
-  const uid = e.target.uid.value;
-  const urlencode = `username=${username}&password=${password}&email=${email}&action=${action}&uid=${uid}`;
-
-  fetch("http://localhost:8000/superclub/newuser", {
-    method: "POST",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    body: encodeURI(urlencode),
-  })
-    .then((res) => {
-      return res.text();
-    })
-    .then((text) => {
-      console.log(text);
-      getCredentials();
-    });
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const email = e.target.email.value;
+    try {
+        const ref = collection(db, "students");
+        if (status === "create") {
+            await addDoc(ref, {
+                username,
+                password,
+                email,
+                facultyid: auth.currentUser.uid,
+            });
+            console.log("success student created");
+        } else {
+            await updateDoc(doc(db, "students", status), {
+                username,
+                password,
+                email,
+            });
+            console.log("student details update");
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        toggleAlert();
+        status = "create";
+    }
 });
 
-// getCredentials();
-
 document.getElementById("createNew").addEventListener("click", () => {
-  const action = document.getElementById("action");
-  action.value = "new";
-  formData();
-  toggleAlert();
+    const action = document.getElementById("action");
+    action.value = "new";
+    formData();
+    toggleAlert();
 });
 
 function alertEsc(evt) {
-  if (evt.code === "Escape") {
-    if (alertBox.style.display === "flex") toggleAlert();
-  }
+    if (evt.code === "Escape") {
+        if (alertBox.style.display === "flex") toggleAlert();
+    }
 }
 
-document.body.addEventListener("keydown",alertEsc)
+document.body.addEventListener("keydown", alertEsc);
+
+document.querySelector("#logoutButn").addEventListener("click",function(){
+  signOut(auth).then(() => {
+    location.href = "/index.html"
+  }).catch((error) => {
+    console.log(error)
+  });
+})
